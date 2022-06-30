495bd43c0c5995d6aafbc5be32feb72d8add7a70
odinx123 <68193880+odinx123@users.noreply.github.com>
2022-06-30 13:58:31 +0000

    Sync LeetCode submission - 3Sum Closest (c)

---
+++ src/problems/3sum_closest/solution.c
@@ -0,0 +1,31 @@
+static int compare(const void *a, const void *b)
+{
+    return *(int *) a - *(int *) b;
+}
+
+int threeSumClosest(int *nums, int numsSize, int target)
+{
+    int min = INT_MAX;
+    int ret = target;
+    qsort(nums, numsSize, sizeof(int), compare);
+    for (int i = 0; i + 2 < numsSize; i++) {
+        int head = i + 1, tail = numsSize - 1;
+        while (tail - head >= 1) {
+            int sum = nums[i] + nums[head] + nums[tail];
+            int com = abs(sum - target);
+            if (com < min) {
+                min = com;
+                ret = sum;
+            }
+
+            if (sum < target)
+                head++;
+            else if (sum > target)
+                tail--;
+            else
+                return target;
+        }
+    }
+
+    return ret;
+}
