a1f821447a45aa6471b444d1d7df3cdbb96fef4c
odinx123 <68193880+odinx123@users.noreply.github.com>
2023-02-20 05:32:15 +0000

    Sync LeetCode submission - Search Insert Position (cpp)

--- src/problems/search_insert_position/solution.cpp
+++ src/problems/search_insert_position/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int beg = 0, end = nums.size()-1;
-        int mid;
-        
-        while (beg <= end) {
-            mid = (beg+end)/2;
-            if (nums.at(mid) == target) {
+        int l = 0, r = nums.size(), mid;
+
+        while (l < r) {
+            mid = (r + l) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            } else if (nums.at(mid) > target) {
-                end = --mid;
+            } else if (nums[mid] > target) {
+                r = mid;
             } else {
-                beg = ++mid;
+                l = mid + 1;
             }
         }
-        
-        return max(beg, end);  // 若是位置小於0則是insert在0(容器都會是insert在位置前)
+
+        // 如果只剩兩個數字mid會靠右邊的數，所以會到這代表只有左邊沒判斷
+        return l;
     }
 };
