dfd733c49af14f9d8aed5705a3479210de7a7fbd
odinx123 <68193880+odinx123@users.noreply.github.com>
2023-10-13 10:41:58 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (cpp)

--- src/problems/min_cost_climbing_stairs/solution.cpp
+++ src/problems/min_cost_climbing_stairs/solution.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) { 
         cost.push_back(0);
+
         for (int i = 2; i < cost.size(); ++i)
             cost[i] += min(cost[i-1], cost[i-2]);
-        
+
         return cost.back();
-    }
-    
-    Solution() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
     }
 };
